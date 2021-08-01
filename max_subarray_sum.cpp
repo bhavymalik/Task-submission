4718a3270cafb920fae4b8ec12bad4efeb9cfc50
bhavymalik <86194537+bhavymalik@users.noreply.github.com>
2021-08-02 00:40:46 +0530

    Add files via upload

---
+++ max_subarray_sum.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int n;
+    cin>>n;
+    cout<<endl;
+    int a[n];
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+    
+    int max=a[0]; 
+    
+    int i,j,k,sum; 
+    
+    for( i=1; i<=n ; i++)
+    {
+        for(j=0; j<= n-i ; j++)
+        {
+            sum= 0;
+            for( k= 0; k<i;k++)
+            sum+= a[j+k] ;
+            
+            if(sum>max)
+            max=sum;
+        }
+    }
+    cout<<endl<<max;
+
+    return 0;
+}
