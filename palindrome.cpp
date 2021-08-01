2de65e710eefcf22955b10429572d4876edf348d
bhavymalik <86194537+bhavymalik@users.noreply.github.com>
2021-08-02 00:55:11 +0530

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+  cout<<"enter the number to check for palindrome : ";
+  int n;
+  cin>>n;
+
+  int x=n;
+  int sum=0,count=0;
+  while(x)
+  {
+    count++;
+    x/=10;
+  }
+
+  x=n;
+
+  while(x)
+  {
+    sum+= pow(x%10,count);
+    x/=10;
+  }
+
+  if(sum==n)
+  cout<<"\nthe number you entered is a palindrome";
+
+  else
+    cout<<"\nthe number you entered is not a palindrome";
+
+    return 0;
+}
